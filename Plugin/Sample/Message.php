527fd6a3c5db10bca1e403bbff786b6d296aa678
Damián Culotta <damianculotta@gmail.com>
2013-02-11 02:22:04 -0300

    Sample plugin

---
+++ Plugin/Sample/Message.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Bender
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Plugin
+ * @package    Sample
+ * @copyright  Copyright (c) 2013 Damián Culotta. (http://www.damianculotta.com.ar/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Plugin\Sample;
+
+class Message {
+
+    public function changeMessage($message) {
+        return $message . " (using my own plugin!)";
+    }
+    
+}
+
+?>
