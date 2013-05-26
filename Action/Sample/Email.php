baa90346535fdb9165fe3fa815f59db2ceaf9f13
Damián Culotta <damianculotta@gmail.com>
2013-05-26 20:13:19 -0300

    Email sample action.

---
+++ Action/Sample/Email.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Bender
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Core
+ * @package    Core
+ * @copyright  Copyright (c) 2013 Damián Culotta. (http://www.damianculotta.com.ar/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Action\Sample;
+
+use Symfony\Component\Console as Console;
+use Bender\Core as Core;
+
+
+class Email extends Core {
+
+    public function __construct($name = null) {
+        parent::__construct($name);
+        $this->setDescription('Test action used on development');
+        $this->setHelp('A simple send mail action example.');
+    }
+
+    protected function execute(Console\Input\InputInterface $input, Console\Output\OutputInterface $output) {
+        $output->writeln(sprintf('Message sent: %s', $this->_getMessage()));
+        /**
+         * CHANGE THE RECIPIENT ADDRESS TO RECIEVE THE EMAIL
+         */
+        $this->_getMailer()->send('recipient@example.com', 'Bender says', sprintf('%s', $this->_getMessage()));
+    }
+
+    private function _getMessage() {
+        return 'Bite my shiny metal ass!';
+    }
+
+}
+
+?>
