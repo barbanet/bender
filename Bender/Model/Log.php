519d2578ef06f0a97922d5c10743df7925b5f731
Damián Culotta <damianculotta@gmail.com>
2014-01-19 23:39:03 -0300

    Logs model.

---
+++ Bender/Model/Log.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Bender
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Core
+ * @package    Core
+ * @copyright  Copyright (c) 2013 Damián Culotta. (http://www.damianculotta.com.ar/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Bender\Model;
+use Bender\Core\Database as Database;
+use \Exception;
+
+class Log {
+    
+    private $_database;
+    
+    public function __construct() {
+        $this->_database = Database::getInstance();
+    }
+    
+    public function all() {
+        $_logs = $this->_database->query('SELECT date, type, activity FROM logs ORDER BY date asc;');
+        return $_logs;
+    }
+    
+}
+
+?>
